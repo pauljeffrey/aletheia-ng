a72c3e40b6a8799400e05abcdc2e9c311cade980
David Okpare <okparedave@gmail.com>
2024-10-13 21:15:43 +0100

    Added additional sections to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { CallToAction } from "@/sections/CallToAction";
+import { CaseStudy } from "@/sections/CaseStudy";
 import { Features } from "@/sections/Features";
+import { Footer } from "@/sections/Footer";
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { Team } from "@/sections/Team";
@@ -10,6 +13,9 @@ export default function Home() {
       <Hero />
       <Features />
       <Team />
+      <CaseStudy />
+      <CallToAction />
+      <Footer />
     </>
 );
 }
